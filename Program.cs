a64a4c573b3a392bb64a0979b7fa7f18ee4cc424
zabronax <zabronax@gmail.com>
2025-04-10 13:19:48 +0200

    setup input handling

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Testing testing!");
+
+// Lese av bruker input
+string? userInput = Console.ReadLine();
+// Sjekk om systemet vi kjører i støtter terminal (ikke alle som gjør det)
+if (userInput == null)
+{
+  throw new Exception("No input found!");
+}
+
+Console.WriteLine(userInput);
+
